f17c1cbbd7572396c1a6fbd8f970b53b84c173a5
Your Name <hr5359@srmist.edu.in>
2021-07-30 13:58:54 +0530

    Study page made

--- src/pages/Study.tsx
+++ src/pages/Study.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Study() {
     return (
         <div>
-            study
+            <h1 className="font-dancingScript pb-2 font-semibold tracking-wide text-5xl lg:text-6xl">
+                Study
+            </h1>
+            <div className="w-72 h-1 rounded-full bg-indigo-500 inline-flex"></div>
         </div>
     )
 }
